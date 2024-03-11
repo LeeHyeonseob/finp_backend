d193215a095c52bde791059b35ab449801eea678
Hyeonseob Lee <97226053+LeeHyeonseob@users.noreply.github.com>
2024-03-11 23:33:53 +0900

    init commit

---
+++ src/main/java/group/finp_backend/FinpBackendApplication.java
@@ -0,0 +1,13 @@
+package group.finp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/finp_backend/FinpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package group.finp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
